8f35a1b30388be2a69f41aaddf666c25d28a9c7d
ana <anabeatrizsouzaalves94@gmail.com>
2025-05-13 11:39:15 -0300

    poo

---
+++ triangulo.cs
@@ -0,0 +1,20 @@
+namespace Triangle
+{
+    public class MinhaClasse
+    {
+// Propriedades
+        public int Propriedade { get; set; }
+
+        public MinhaClasse()
+        {
+            Propriedade = 0;
+        }
+
+        public void MeuMetodo()
+        {
+            Console.WriteLine("Método chamado!");
+        }
+    }
+}
+
+
